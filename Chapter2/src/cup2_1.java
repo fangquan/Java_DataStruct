f7eaf6b25f00ac169b5946a1dfc74d2d3e055414
fangquan <fangquans@gmail.com>
2012-11-22 00:26:54 -0500

    Chapter 2 re-commit

---
+++ Chapter2/src/cup2_1.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+
+public class cup2_1 {
+
+	static int Max_Value = 10;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LinkedList <String> list = new LinkedList<String>();
+		
+		for (int i=0; i<Max_Value;i++) {
+			String r = Integer.toString((int) (Math.random()*Max_Value));
+			list.add(r);
+		}
+		
+		
+		System.out.println(list);
+		
+	}
+
+}
